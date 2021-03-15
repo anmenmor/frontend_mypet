9a7b7bc59571c654bd5059e687844dc29f729b48
Laura <lauriitaa_93_@hotmail.com>
2021-03-15 19:03:59 +0100

    añadiendo estructura componentes

---
+++ src/app/components/employees/employees.module.ts
@@ -0,0 +1,18 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { EmployeesLoginComponent } from '.\employees-login\employees-login.component';
+// import { logging } from 'selenium-webdriver';
+
+// // const routes: Routes = [
+// //   { path: 'login', component: EmployessLoginComponent}
+// // ];
+
+// @NgModule({
+//   declarations: [
+//     EmployeesLoginComponent
+//   ],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class EmployeesModule { }
