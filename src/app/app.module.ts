f174c06e318652ee81d5bab329c2c2ab912a6cb1
fcoschzcr <fcoschzcr@gmail.com>
2021-03-16 11:23:20 +0100

    Progreso componente info clinicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesLoginComponent } from './components/employees/employees-login/employees-login.component';
 import { EmployeesRegisterComponent } from './components/employees/employees-register/employees-register.component';
+import { ClinicsDataComponent } from './components/clinics-data/clinics-data.component';
+import { ClinicsDataService } from './components/clinics-data/clinics-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesLoginComponent,
-    EmployeesRegisterComponent
+    EmployeesRegisterComponent,
+    ClinicsDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ClinicsDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
