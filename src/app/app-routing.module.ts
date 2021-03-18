03c6dd700ca8e32ba3eeb1bb5ebddf7c99510b7b
fcoschzcr <fcoschzcr@gmail.com>
2021-03-18 13:42:48 +0100

    Componente edit clinics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeesLoginComponent } from './components/employees/employees-login/employees-login.component';
 import { EmployeesRegisterComponent } from  './components/employees/employees-register/employees-register.component';
-import { ClinicsDataComponent } from './clinics/components/clinics-data/clinics-data.component';
+import { ClinicsListComponent } from './clinics/components/clinics-list/clinics-list.component';
+import { ClinicsUpdateComponent } from './clinics/components/clinics-update/clinics-update.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
 
 const routes: Routes = [
   {path: 'loginEmployee', component:EmployeesLoginComponent, pathMatch: 'full'},
   {path: 'registerEmployee', component:EmployeesRegisterComponent, pathMatch: 'full'},
-  {path: 'clinicsData', component:ClinicsDataComponent, pathMatch: 'full'},
+  {path: 'clinicsList', component:ClinicsListComponent, pathMatch: 'full'},
+  {path: 'clinicsUpdate', component:ClinicsUpdateComponent, pathMatch: 'full'},
   {path: 'employees', component:EmployeesListComponent, pathMatch: 'full'},
 ];
 
