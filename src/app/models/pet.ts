45319f7680d6669d3cb0555b80a63d40d1ff868f
anmenmor <manendezandrea86@gmail.com>
2021-03-21 15:17:09 +0100

    create pet component

---
+++ src/app/models/pet.ts
@@ -0,0 +1,11 @@
+export interface Pet{
+    id: number;
+    name: string;
+    sex: string;
+    weight: number;
+    age: number;
+    species: string;
+    clientId: number;
+    breed: string;
+    available: boolean;
+}
