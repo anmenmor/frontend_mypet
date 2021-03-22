9dbc4dce9660154545ac4247abc11ac0eb8d7732
monicarivi <momokobcn@gmail.com>
2021-03-22 19:10:24 +0100

    clients

---
+++ src/app/models/clients.ts
@@ -0,0 +1,12 @@
+export class Clients{
+    constructor(
+        public id: number,
+        public name: string,
+        public surname: string,
+        public email: string,
+        public password: string,
+        public phone: number
+    ){
+
+    }
+}
