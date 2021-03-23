b24ed37cf9d35df06e847810d972906ec2f10504
fcoschzcr <fcoschzcr@gmail.com>
2021-03-23 11:55:29 +0100

    Retoques

---
+++ src/app/components/clinics/clinics-list/clinics-list.component.ts
@@ -0,0 +1,49 @@
+import { Component } from "@angular/core";
+import { ClinicsDataService } from "../../../services/clinics-data.service";
+
+@Component({
+  selector: "app-clinics-list",
+  templateUrl: "./clinics-list.component.html",
+  styleUrls: ["./clinics-list.component.css"],
+})
+export class ClinicsListComponent {
+  clinics = [] as any;
+  clinic = [] as any;
+  clinicId = 0;
+
+  constructor(private clinicsDataService: ClinicsDataService) {}
+
+  listAllClinics() {
+    this.clearPreviousValues();
+    this.clinicsDataService.listAllClinics().subscribe((data) => {
+      for (const d of data as any) {
+        this.clinics.push({
+          name: d.name,
+          city: d.city,
+          address: d.address,
+          phone: d.phone,
+          email: d.email,
+        });
+      }
+    });
+  }
+
+  listSingleClinic(clinicId: number) {
+    this.clearPreviousValues();
+    this.clinicsDataService.listSingleClinic(clinicId).subscribe(
+      (data) =>
+        (this.clinic = {
+          name: (data as any).name,
+          city: (data as any).city,
+          address: (data as any).address,
+          phone: (data as any).phone,
+          email: (data as any).email,
+        })
+    );
+  }
+
+  clearPreviousValues(){
+    this.clinic = [];
+    this.clinics = [];
+  }
+}
---
+++ src/app/components/clinics/clinics-update/clinics-update.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+import { Clinic } from "src/app/models/clinic.model";
+import { ClinicsDataService } from "../../../services/clinics-data.service";
+
+@Component({
+  selector: "app-clinics-update",
+  templateUrl: "./clinics-update.component.html",
+  styleUrls: ["./clinics-update.component.css"],
+})
+export class ClinicsUpdateComponent {
+  postBody = new Clinic();
+  clinicId = 0;
+  htmlMsg!: String;
+
+  constructor(private clinicsDataService: ClinicsDataService) {}
+
+  onSubmit(data: any) {
+    if (this.clinicId > 0) {
+      this.postBody = data;
+      this.clinicsDataService
+        .updateClinic(this.clinicId, this.postBody)
+        .subscribe(
+          (data) => this.htmlMsg = "Datos de clinica modificados correctamente",
+          (exception) => this.htmlMsg = exception.error.message
+        );
+    } else {
+      this.htmlMsg = "Introduce un ID";
+    }
+  }
+}
