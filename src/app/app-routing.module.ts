09db9e040218b00b34a2a6cd535db8bf786df334
fcoschzcr <fcoschzcr@gmail.com>
2021-03-23 11:58:45 +0100

    Retoques

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeesLoginComponent } from './components/employees/employees-login/employees-login.component';
 import { EmployeesRegisterComponent } from  './components/employees/employees-register/employees-register.component';
-import { ClinicsListComponent } from './clinics/components/clinics-list/clinics-list.component';
-import { ClinicsUpdateComponent } from './clinics/components/clinics-update/clinics-update.component';
+import { ClinicsListComponent } from './components/clinics/clinics-list/clinics-list.component';
+import { ClinicsUpdateComponent } from './components/clinics/clinics-update/clinics-update.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 import { VaccinesComponent } from './components/vaccines/vaccines.component';
 import { CreatePetComponent } from './components/create-pet/create-pet.component';
@@ -11,6 +11,7 @@ import { ListPetsComponent } from './components/list-pets/list-pets.component';
 import { ClientsLoginComponent } from './components/clients/clients-login/clients-login.component';
 import { ClientsRegisterComponent } from  './components/clients/clients-register/clients-register.component';
 import { ClientsListComponent } from './components/clients/clients-list/clients-list.component';
+import { VaccinationsComponent } from './components/vaccinations/vaccinations.component';
 
 
 const routes: Routes = [
@@ -24,7 +25,8 @@ const routes: Routes = [
   {path: 'clients/:clientId/pets', component: ListPetsComponent, pathMatch: 'full'},
   {path: 'loginClients', component:ClientsLoginComponent, pathMatch: 'full'},
   {path: 'registerClients', component:ClientsRegisterComponent, pathMatch: 'full'},
-  {path: 'clients', component:ClientsListComponent, pathMatch: 'full'}
+  {path: 'clients', component:ClientsListComponent, pathMatch: 'full'},
+  {path: 'vaccinations', component:VaccinationsComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
