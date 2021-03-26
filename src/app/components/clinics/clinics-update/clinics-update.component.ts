c1e43b358bbc5ca173fadadaf1eb763fab2b28b2
fcoschzcr <fcoschzcr@gmail.com>
2021-03-26 18:29:39 +0100

    Retoques

--- src/app/components/clinics/clinics-update/clinics-update.component.ts
+++ src/app/components/clinics/clinics-update/clinics-update.component.ts
@@ -1,21 +1,57 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Clinic } from "src/app/models/clinic.model";
 import { ClinicsDataService } from "../../../services/clinics-data.service";
+import { FormBuilder, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-clinics-update",
   templateUrl: "./clinics-update.component.html",
   styleUrls: ["./clinics-update.component.css"],
 })
-export class ClinicsUpdateComponent {
+export class ClinicsUpdateComponent implements OnInit {
   postBody = new Clinic();
+  clinicUpdate;
+  clinics: any;
+  clinic: any;
   clinicId = 0;
   htmlMsg!: String;
 
-  constructor(private clinicsDataService: ClinicsDataService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private clinicsDataService: ClinicsDataService
+  ) {
+    this.clinicUpdate = this.formBuilder.group({
+      id: -1,
+      name: "",
+      city: "",
+      address: "",
+      phone: "",
+      email: "",
+    });
+  }
 
-  onSubmit(data: any) {
-    if (this.clinicId > 0) {
+  ngOnInit() {
+    this.clinicsDataService
+      .listAllClinics()
+      .subscribe((data) => (this.clinics = data));
+  }
+
+  onChange(e: number) {
+    this.clinicId = e;
+    this.clinicsDataService.listSingleClinic(e).subscribe(
+      (data) =>
+        (this.clinic = {
+          name: (data as any).name,
+          city: (data as any).city,
+          address: (data as any).address,
+          phone: (data as any).phone,
+          email: (data as any).email,
+        })
+    );
+  }
+
+  onSubmit(data: Clinic) {
+    console.log(data);
       this.postBody = data;
       this.clinicsDataService
         .updateClinic(this.clinicId, this.postBody)
@@ -24,8 +60,5 @@ export class ClinicsUpdateComponent {
             (this.htmlMsg = "Datos de clinica modificados correctamente"),
           (exception) => (this.htmlMsg = exception.error.message)
         );
-    } else {
-      this.htmlMsg = "Introduce un ID";
-    }
   }
 }
