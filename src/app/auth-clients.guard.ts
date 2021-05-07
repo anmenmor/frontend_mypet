0648e24f5049fb9fd05617af81033224722fbe91
lcheca393 <lauriitaa_93_@hotmail.com>
2021-05-07 13:57:19 +0200

    employees and clients guards

--- src/app/auth-clients.guard.ts
+++ src/app/auth-clients.guard.ts
@@ -1,28 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { TokenEmployeeService } from './shared/token-employee.service';
+import { TokenClientsService } from './shared/token-clients.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthEmployeesGuard implements CanActivate {
-  
+export class AuthClientsGuard implements CanActivate {
+
   constructor(
     private router: Router,
-    private tokenEmployee: TokenEmployeeService
+    private tokenClients: TokenClientsService
   ) { }
 
   canActivate() {
-    const currentPayload = this.tokenEmployee.payload();
+    const currentPayload = this.tokenClients.payload();
     const loginUrl: String = currentPayload ? currentPayload.iss : "";
-    if (loginUrl.endsWith("loginEmployee")) {
+    if (loginUrl.endsWith("loginClients")) {
       return true;
     }
     this.router.navigate(['/']);
     return false;
 
   }
-
-
-
+  
 }
